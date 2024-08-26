b2ace11b52792f60df63fdcb492b802c6cc34d88
Kasper Høg <mailkasperhog@gmail.com>
2024-08-26 08:26:12 +0200

    made sign in set jwt cookie

--- frontend/app/sign-in/actions.ts
+++ frontend/app/sign-in/actions.ts
@@ -3,6 +3,7 @@
 import { z } from "zod";
 import { typeSchemas, usernameSchema } from "../plugins/Form/schemas";
 import { isRedirectError } from "next/dist/client/components/redirect";
+import { cookies } from "next/headers";
 import axiosInstance from "@/lib/api";
 
 const signInSchema = z.object({
@@ -28,7 +29,20 @@ export async function signIn(formData: SignUpFormData) {
   const data = validation.data;
 
   try {
-    await axiosInstance.post("/users/login/", data);
+    const reponse = await axiosInstance.post("/users/login/", data);
+
+    const cookiesReponse = reponse.headers["set-cookie"];
+
+    if (cookiesReponse) {
+      const jwt = cookiesReponse[0]
+        .split(";")
+        .find((c) => c.startsWith("jwt="))
+        ?.split("=")[1];
+
+      if (jwt) {
+        cookies().set("jwt", jwt);
+      }
+    }
 
     return {
       success: "You have successfully signed up!",
