430726ef2e03f4f41e10a7c7143e2d7b03602bad
Kasper Høg <mailkasperhog@gmail.com>
2024-08-26 07:59:41 +0200

    created user auth for dashboard + redirect

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { redirect } from "next/navigation";
+import axiosInstance from "@/lib/api";
 
 import SideBar from "@/components/Sidebar";
 import Header from "@/components/Header";
@@ -10,16 +12,29 @@ interface DashboardLayoutProps {
 export default async function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
+  let user: User | null = null;
+
+  try {
+    const response = await axiosInstance.get("/users/current/");
+    user = response.data;
+  } catch (error) {
+    console.error("Error while fetching user data", error);
+  }
+
+  if (!user) {
+    redirect("/sign-up");
+  }
+
   return (
     <div className="relative h-full overflow-hidden bg-background">
-      <SideBar />
+      <SideBar user={user} />
       <main
         id="content"
-        className="overflow-x-hidden pt-16 transition-[margin] md:overflow-y-hidden md:pt-0 md:ml-64 h-full"
+        className="overflow-x-hidden pt-16 transition-[margin] md:overflow-y-hidden md:pt-0 md:ml-64 bg-card"
       >
-        <div className="relative flex h-full flex-col">
-          <Header />
-          <div className="flex-1 overflow-hidden px-4 py-6 md:px-8 bg-secondary">
+        <div className="relative flex min-h-screen flex-col">
+          <Header user={user} />
+          <div className="flex-1 overflow-hidden px-4 py-6 md:px-8 h-full bg-secondary">
             {children}
           </div>
         </div>
