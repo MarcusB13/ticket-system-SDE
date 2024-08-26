f38d01b21acfa70a6876514e9055f8a010f37118
Kasper Høg <mailkasperhog@gmail.com>
2024-08-26 08:01:19 +0200

    added user to sidebar and usermenu

--- frontend/components/Header/UserMenu.tsx
+++ frontend/components/Header/UserMenu.tsx
@@ -9,13 +9,17 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { CiSettings } from "react-icons/ci";
 import { IoPowerOutline } from "react-icons/io5";
 import { useRouter } from "next/navigation";
 
-export default function UserMenu({ user }: any) {
+interface UserMenuProps {
+  user: User;
+}
+
+export default function UserMenu({ user }: UserMenuProps) {
   const router = useRouter();
 
   return (
@@ -23,15 +27,14 @@ export default function UserMenu({ user }: any) {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="" alt={user.name} />
-            <AvatarFallback>KH</AvatarFallback>
+            <AvatarFallback className="uppercase">{user.username.slice(0, 1)}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{user.name}</p>
+            <p className="text-sm font-medium leading-none">{user.username}</p>
             <p className="text-xs leading-none text-muted-foreground">
               {user.email}
             </p>
