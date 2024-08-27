71cbde264aa429f9f9185d561a241c6415bfc963
Kasper Høg <mailkasperhog@gmail.com>
2024-08-27 08:26:49 +0200

    added and updated more types

---
+++ frontend/types/pagination.d.ts
@@ -0,0 +1,5 @@
+interface Pagination {
+  count: number;
+  next: string | null;
+  previous: string | null;
+}
---
+++ frontend/types/service_level_agreement.d.ts
@@ -0,0 +1,7 @@
+interface ServiceLevelAgreement {
+  product: string;
+  max_response_time: number;
+  max_resolution_time: number;
+  is_accepted: boolean;
+  company: Company;
+}
