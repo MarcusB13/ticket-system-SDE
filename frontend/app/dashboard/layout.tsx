76251cf302b0afd2f141a66f49225803a9cc439d
Kasper Høg <mailkasperhog@gmail.com>
2024-08-29 11:08:49 +0200

    fix dynamic cookies error and nextjs update

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -9,6 +9,8 @@ interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
+export const dynamic = 'force-dynamic';
+
 export default async function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
