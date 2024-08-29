847535db5d43cf9d453905eeebefb5fd27a3d974
Kasper Høg <mailkasperhog@gmail.com>
2024-08-29 09:00:11 +0200

    added edit users page and action

---
+++ frontend/app/dashboard/users/[id]/actions.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { z } from "zod";
+import { usernameSchema, emailSchema } from "@/app/plugins/Form/schemas";
+import axiosInstance from "@/lib/api";
+
+interface FormData {
+  uuid: string;
+  username: string;
+  email: string;
+  role: string;
+  company: string;
+  is_active: boolean;
+  isCurrentUser: boolean;
+}
+
+const updateUserScheme = z.object({
+  uuid: z.string(),
+  username: usernameSchema,
+  email: emailSchema,
+  role: z.string(),
+  company: z.string(),
+  is_active: z.boolean(),
+  isCurrentUser: z.boolean(),
+});
+
+export async function updateUser(formData: FormData) {
+  const validation = updateUserScheme.safeParse(formData);
+
+  if (!validation.success) {
+    return {
+      error: "Missing one or more fields",
+      status: 400,
+    };
+  }
+
+  const { uuid, ...data } = validation.data;
+
+  try {
+    if (data.isCurrentUser) {
+      await axiosInstance.patch("/users/update/", data);
+    } else {
+      await axiosInstance.patch(`/users/${uuid}/`, {
+        ...data,
+        company: [data.company],
+      });
+    }
+
+    return {
+      success: "User updated successfully!",
+      status: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      error: "Something went wrong",
+      status: 500,
+    };
+  }
+}
--- frontend/app/dashboard/users/[id]/page.tsx
+++ frontend/app/dashboard/users/[id]/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import axiosInstance from "@/lib/api";
+import { Form, Control, Button } from "@/app/plugins/Form";
+import { notFound } from "next/navigation";
+import { updateUser } from "./actions";
 
 interface Props {
   params: {
@@ -7,10 +10,110 @@ interface Props {
   };
 }
 
-export default function page({ params }: Props) {
+export default async function page({ params }: Props) {
   const id = params.id;
 
-  
+  let user: User | null = null;
+  let isCurrentUser: boolean = false;
+  let companies: Company[] = [];
 
-  return <div>page</div>;
+  const currentUserResponse = await axiosInstance.get("/users/current/");
+
+  const currentUser: User = currentUserResponse.data;
+
+  if (currentUser.uuid !== id) {
+    const userResponse = await axiosInstance.get(`/users/${id}/`);
+
+    user = userResponse.data;
+  } else {
+    user = currentUser;
+    isCurrentUser = true;
+  }
+
+  if (!user) {
+    notFound();
+  }
+
+  const isAdmin = currentUser.role === "admin";
+
+  if (isAdmin) {
+    const companiesResponse = await axiosInstance.get("/tickets/companies/");
+    companies = companiesResponse.data;
+  }
+
+  return (
+    <div className="flex justify-center">
+      <Form
+        item={{
+          ...user,
+          company: user.company[0]?.uuid,
+          isCurrentUser,
+        }}
+        onSubmit={updateUser}
+        className="rounded-lg border bg-card text-card-foreground shadow-sm mx-auto w-3/4"
+      >
+        <div className="flex flex-col p-6 space-y-1">
+          <h3 className="whitespace-nowrap tracking-tight font-bold text-2xl">
+            Edit user
+          </h3>
+          <p className="text-sm text-muted-foreground">
+            Edit user details for user ID: {user.pk}
+          </p>
+        </div>
+        <div className="p-6 pt-0">
+          <div className="space-y-6">
+            <div className="gap-2 grid grid-cols-2">
+              <Control
+                type="username"
+                placeholder="Username"
+                label="Username"
+                name="username"
+                required
+              />
+              <Control
+                type="text"
+                placeholder="Email"
+                label="Email"
+                name="email"
+                required
+                disabled
+                customSchema="email"
+              />
+            </div>
+            <Control
+              type="select"
+              name="role"
+              label="Role"
+              options={[
+                "admin",
+                "user",
+                "supporter",
+                "it-supporter",
+                "developer",
+              ].map((role) => ({
+                label: role.charAt(0).toUpperCase() + role.slice(1),
+                value: role,
+              }))}
+              disabled={!isAdmin}
+            />
+            {isAdmin && (
+              <>
+                <Control
+                  type="select"
+                  name="company"
+                  label="Company"
+                  options={companies.map((company) => ({
+                    label: company.name,
+                    value: company.uuid,
+                  }))}
+                />
+                <Control type="checkbox" name="is_active" label="Active" />
+              </>
+            )}
+            <Button text="Save" className="w-full" />
+          </div>
+        </div>
+      </Form>
+    </div>
+  );
 }
